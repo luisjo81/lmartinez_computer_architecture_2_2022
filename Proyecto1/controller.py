22ae95bd4ffc182bf69f61deac160a83f23ecb91
Luis Martínez Ramírez <louisjo81@gmail.com>
2022-09-18 05:10:33 -0600

    Starting state machine

--- Proyecto1/controller.py
+++ Proyecto1/controller.py
@@ -1,10 +1,16 @@
+import Memory
+import Processor
+import Gui
+
 import threading
 import time
 
 class Controller:
     def __init__(self):
         self.wait_time = 3
+        memory = Memory()
 
+    """
     def threads_manager(self):
         p0_thread = threading.Thread(target = processor, args = ())
         p1_thread = threading.Thread(target = processor, args = ())
@@ -16,3 +22,16 @@ class Controller:
             p1_thread.start() 
             p2_thread.start() 
             p3_thread.start()
+    """
+
+    #Function to change state in cache block
+    def change_state(self, processor, current_state, action, address, value):
+        if current_state == "I":
+            if action == "WRITE":
+                print("Processor " + str(processor) + ": Write Miss")
+                memory.write_to_cache(address, processor, value)
+                print("Processor " + str(processor) + " State: Changing from I to M")
+                memory.change_cache_block_state(processor, address)
+
+                
+                
